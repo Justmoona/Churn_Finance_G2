2eb0b67e5bfa03a64c675b89487fa6cfa7a7b5d0
bouboss <marytra22@gmail.com>
2024-03-01 05:52:18 +0000

    scalling

--- app/app.py
+++ app/app.py
@@ -8,7 +8,8 @@ import pandas as pd
 app=Flask(__name__)
 
 # Charger le modele
-rfcmodel=pickle.load(open('rfcmodel.pkl','rb'))
+rfcmodel=pickle.load(open('model/rfcmodel.pkl','rb'))
+scalar=pickle.load(open('model/scaling.pkl','rb'))
 
 @app.route('/')
 def index():
@@ -19,7 +20,7 @@ def predict_api():
     data=request.json['data']
     print('Donnees renvoyer: => {}'.format(data))
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=np.array(list(data.values())).reshape(1,-1)
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     print('Variables d\'entrees: => {}'.format(new_data))
     output=rfcmodel.predict(new_data)
     print('Output du model => {}'.format(output[0]))
