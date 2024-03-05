8d417c776280936751dbe3578731510bcc4cd225
bouboss <marytra22@gmail.com>
2024-03-05 02:28:52 +0000

    test

--- app/collect/collect.py
+++ app/collect/collect.py
@@ -6,10 +6,10 @@ print("==============Downloading data=============")
 url = "https://drive.google.com/uc?id=1IGFx1FgzIYfEIq3viV10tx2sG_36YgUm"
 gdown.download(url)
 
-# Créer un repertoire nomer data dans app
-os.mkdir('app/data')
+# # Créer un repertoire nomer data dans app
+# os.mkdir('app/data')
 
-# Déplacer le fichier csv vers le dossier data
-shutil.move('./churn.csv', './app/data/churn.csv')
+# # Déplacer le fichier csv vers le dossier data
+# shutil.move('./churn.csv', './app/data/churn.csv')
 
 
